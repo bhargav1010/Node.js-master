53bc47ab8b288a850f490c6303b9e7d0ddf930c8
Bhargav <badiya.bhargav@aquera.com>
2023-04-15 18:28:26 +0530

    sending data to a file

--- Notes.js
+++ Notes.js
@@ -10,7 +10,7 @@ server.listen(3500);
 
 
 
-//code:1
+//code-1
 const http=require('http');
 const server=http.createServer((req,res)=>{
     // console.log(req.url,req.method,req.headers);
@@ -28,6 +28,48 @@ const server=http.createServer((req,res)=>{
     if(url==='/welcome_page'){
         res.write('<html>');
         res.write('<head><h1>Welcome to Node.js</h1></head>')
+        res.write('</html>');
+        return res.end();
+    }
+
+})
+
+server.listen(4200);
+
+
+
+//code-2
+
+const http=require('http');
+const fs=require('fs');
+const server=http.createServer((req,res)=>{
+    // console.log(req.url,req.method,req.headers);
+    const url=req.url;
+    const method=req.method;
+    
+    if(url==='/'){
+    // setTimeout(()=>{process.exit();},5000);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<title>HTML</title>');
+    res.write('<head><h1>Click Get button to get Welcom page</h1></head>')
+    res.write('<body><form action="/data_sent_to_file" method="POST"><input type=text name=data><button type="submit">Send Data</button></form></body>');
+    res.write('</html>');
+    }
+    if(url==='/data_sent_to_file' && method==="POST"){
+        const body=[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        })
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString();
+            data=parsedBody.split('=')[1];
+            console.log(data);
+            fs.writeFileSync('dataFile.txt',data);
+        })
+        res.write('<html>');
+        res.write('<head><h1>Data Sent Successfully</h1></head>')
         res.write('</html>');
         return res.end();
     }
