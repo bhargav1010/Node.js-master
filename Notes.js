bd4ca4eee532438d5ef6dffa94a2e7a2fccb37e8
Bhargav <badiya.bhargav@aquera.com>
2023-04-15 18:02:10 +0530

    url

--- Notes.js
+++ Notes.js
@@ -7,3 +7,31 @@ const server=http.createServer((req,res)=>{
 })
 //listening to server
 server.listen(3500);
+
+
+
+//code:1
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    // console.log(req.url,req.method,req.headers);
+    const url=req.url;
+    
+    if(url==='/'){
+    // setTimeout(()=>{process.exit();},5000);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<title>HTML</title>');
+    res.write('<head><h1>Click Get button to get Welcom page</h1></head>')
+    res.write('<body><form action="/welcome_page" method="POST"><button type="submit">Get</button></form></body>');
+    res.write('</html>');
+    }
+    if(url==='/welcome_page'){
+        res.write('<html>');
+        res.write('<head><h1>Welcome to Node.js</h1></head>')
+        res.write('</html>');
+        return res.end();
+    }
+
+})
+
+server.listen(4200);
--- new-1.js
+++ new-1.js
@@ -1,8 +1,24 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+    // console.log(req.url,req.method,req.headers);
+    const url=req.url;
+    
+    if(url==='/'){
+    // setTimeout(()=>{process.exit();},5000);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<title>HTML</title>');
+    res.write('<head><h1>Click Get button to get Welcom page</h1></head>')
+    res.write('<body><form action="/welcome_page" method="POST"><button type="submit">Get</button></form></body>');
+    res.write('</html>');
+    }
+    if(url==='/welcome_page'){
+        res.write('<html>');
+        res.write('<head><h1>Welcome to Node.js</h1></head>')
+        res.write('</html>');
+        return res.end();
+    }
 
-    setTimeout(()=>{process.exit();},5000);
 })
 
-server.listen(3500);
+server.listen(4200);
