d257c7d01824f5c4d102c5e2baafd5ef6e511e06
Bhargav <badiya.bhargav@aquera.com>
2023-04-16 22:01:02 +0530

    by exporting code

--- new-1.js
+++ new-1.js
@@ -1,37 +1,4 @@
 const http=require('http');
-const fs=require('fs');
-const server=http.createServer((req,res)=>{
-    // console.log(req.url,req.method,req.headers);
-    const url=req.url;
-    const method=req.method;
-    
-    if(url==='/'){
-    // setTimeout(()=>{process.exit();},5000);
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<title>HTML</title>');
-    res.write('<head><h1>Enter the data you want to send to the dataFile</h1></head>')
-    res.write('<body><form action="/data_sent_to_file" method="POST"><textarea type=text name=data></textarea><button type="submit">Send Data</button></form></body>');
-    res.write('</html>');
-    }
-    if(url==='/data_sent_to_file' && method==="POST"){
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        })
-        req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString();
-            data=parsedBody.split('=')[1];
-            console.log(data);
-            fs.writeFileSync('dataFile.txt',data);
-        })
-        res.write('<html>');
-        res.write('<head><h1>Data Sent Successfully</h1></head>')
-        res.write('</html>');
-        return res.end();
-    }
-
-})
-
+const routes= require('./routes');
+server = http.createServer(routes.handler);
 server.listen(4200);
---
+++ routes.js
@@ -0,0 +1,39 @@
+const fs=require('fs');
+const requestHandler=(req,res)=>{
+    // console.log(req.url,req.method,req.headers);
+    const url=req.url;
+    const method=req.method;
+
+    if(url==='/'){
+    // setTimeout(()=>{process.exit();},5000);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<title>HTML</title>');
+    res.write('<head><h1>Enter the data you want to send to the dataFile</h1></head>')
+    res.write('<body><form action="/data_sent_to_file" method="POST"><textarea type=text name=data></textarea><button type="submit">Send Data</button></form></body>');
+    res.write('</html>');
+    }
+    if(url==='/data_sent_to_file' && method==="POST"){
+        const body=[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        })
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString();
+            data=parsedBody.split('=')[1];
+            console.log(data);
+            fs.writeFileSync('dataFile.txt',data);
+        })
+        res.write('<html>');
+        res.write('<head><h1>Data Sent Successfully</h1></head>')
+        res.write('</html>');
+        return res.end();
+    }
+
+}
+
+module.exports={
+    handler:requestHandler,
+    someText:'Hardcoded Text'
+}
